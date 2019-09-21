497b7295f9e531ab82be15b6f41d8c0ce7e58d3f
one1on <x7472000-11@tutanota.com>
2019-09-21 12:24:31 +0200

    Fixed bug in hashtag extraction

--- src/main/java/de/one1on/sgjscraper/core/HashTagExtractor.java
+++ src/main/java/de/one1on/sgjscraper/core/HashTagExtractor.java
@@ -12,7 +12,7 @@ public class HashTagExtractor {
     private static final Function<String, String> normalize = s -> Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 
     public static String extract(List<String> lines) {
-        Pattern pattern = Pattern.compile("(?i)<a?.+>#(.+?)</a>");
+        Pattern pattern = Pattern.compile("(?i)<a.*?>#(.+?)</a>");
         return lines.stream().map(pattern::matcher)
                     .filter(Matcher::find)
                     .map(m -> m.group(1))
